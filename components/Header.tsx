442169a91b5e69eb67d2510e8ad39dad6f109a35
as0ne <as.sidikousoumana.as@gmail.com>
2023-12-06 12:46:41 +0100

    Fix some bugs.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MenuIcon, SearchIcon } from "@/public/icons";
 import React from "react";
 import Avatar from "./Avatar";
@@ -6,7 +8,10 @@ const Header = () => {
   return (
     <div className="w-full flex items-center justify-between p-8">
       <div className="flex items-center gap-8 flex-1">
-        <button>
+        <button
+          className="flex items-center"
+          onClick={() => alert("Menu appear")}
+        >
           <MenuIcon
             fill="#14b8a6"
             className="bg-white rounded-md w-12 h-12 lg:hidden"
